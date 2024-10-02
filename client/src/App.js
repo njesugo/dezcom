cfa45a6814bb93b2d7003701abda5df85ab3b8d1
Jesugo <jesugo@Air-de-Jesugo.lan>
2024-10-02 20:04:39 +0200

    ignoring nd_mod

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
