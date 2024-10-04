618493ab89fdfc2005cf1d3696171a73e60ed382
Jesugo <jesugo@MacBook-Air-de-Jesugo.local>
2024-10-04 03:16:22 +0200

    added models routes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,25 @@
-
 const express = require("express");
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+const dotenv = require("dotenv");
+const userRouter = require("./src/routes/user");
+
+dotenv.config();
 
 app.get("/api", (req, res)=>{
     res.json({message : "Hello from server!"});
 });
+
+const mongoose = require("mongoose");
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(()=>{console.log("Successfully connected to MongoDB");})
+    .catch((err)=>{console.log(err);})
+
+app.use(express.json());
+
+
+app.use("/api/users/", userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port : ${PORT}`);
